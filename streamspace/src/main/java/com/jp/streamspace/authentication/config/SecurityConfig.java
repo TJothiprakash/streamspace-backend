c186f7377259bcb522d336b9fbbfbcbe63630fc3
Jothiprakash T <jothiprakash888@gmail.com>
2025-10-16 20:29:28 +0530

    Update CORS configuration to allow new origin

--- streamspace/src/main/java/com/jp/streamspace/authentication/config/SecurityConfig.java
+++ streamspace/src/main/java/com/jp/streamspace/authentication/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://your-app.onrender.com"
+                "https://streamspace-ui.onrender.com"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
